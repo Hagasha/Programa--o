1dc03e6bfb08dff985a77becd319f7998bb3f74e
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-06-26 19:49:33 -0300

    Aprendendo javascript

---
+++ javascript/learn.js
@@ -0,0 +1,7 @@
+let fullName = "John Doe";
+let age = 25;
+let isMarried = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `You are married: ${isMarried}`;
