f913b93303761ddf1d8b903889603d221c782a76
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-02 16:45:10 -0300

    aprendendo o objeto math

--- javascript/learn.js
+++ javascript/learn.js
@@ -1,10 +1,6 @@
-const PI = 3.14159;
-let raio; 
-let circunferencia;
+let x = 3;
+let y = 2;
+let z = 1;
 
-document.getElementById("meuenvio").onclick = function() {
-    raio = document.getElementById("meutexto").value;
-    raio = Number(raio);
-    circunferencia = 2 * PI * raio;
-    document.getElementById("meuH3").textContent = circunferencia + "cm";
-};
+let max = Math.min(x, y, z);
+console.log(max);
