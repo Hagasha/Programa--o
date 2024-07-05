b75e360e0b04e3a932e1348c4954879b3282919f
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-04 21:10:44 -0300

    feat: Adicionar funcionalidade de validação de idade no formulário de inscrição

--- javascript/learn.js
+++ javascript/learn.js
@@ -1,6 +1,25 @@
-let x = 3;
-let y = 2;
-let z = 1;
+const meutexto = document.getElementById('meutexto');
+const meuenvio = document.getElementById('meuenvio');
+const meuresultado = document.getElementById('meuresultado');
+let age = 0;
 
-let max = Math.min(x, y, z);
-console.log(max);
+meuenvio.onclick = function(){
+    age = meutexto.value;
+    age = Number(age);
+
+    if (age >= 100) {
+    meuresultado.textContent = "Você é um centenário!";
+    }
+    else if (age == 0) {
+    meuresultado.textContent = "Você acabou de nascer, não pode acessar o site!";
+    }
+    else if (age >= 18) {
+    meuresultado.textContent = "Você pode acessar o site!";
+    }
+    else if(age < 0) {
+    meuresultado.textContent = "Sua idade não pode ser negativa!";
+    }
+    else {  
+    meuresultado.textContent = "Você deve ter mais de 18 anos para acessar o site!";
+    }
+}
