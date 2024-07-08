e623ece10e43d02a96c2149fede15fd436f2661f
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-08 18:05:14 -0300

    chore: Remove unnecessary form elements and update JavaScript code

--- javascript/learn.js
+++ javascript/learn.js
@@ -1,9 +1,7 @@
-let userName = "John Doe";
-let result = userName.includes(" ");
+// let numeroTelefone = "123-456-789";
+// numeroTelefone = numeroTelefone.replaceAll("-","");
+// console.log(numeroTelefone);
 
-if(result){
-    console.log("Your username cannot include a space");
-    }
-else{
-    console.log(userName);
-    }
+let numeroTelefone = "123-456-789";
+numeroTelefone = numeroTelefone.padEnd(15, "333");
+console.log(numeroTelefone);
