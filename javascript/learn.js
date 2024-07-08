851aa845fecc502ada9f87e67b5f0e686d6557be
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-08 16:46:13 -0300

    chore: Remove unnecessary form elements and update JavaScript code

--- javascript/learn.js
+++ javascript/learn.js
@@ -1,51 +1,9 @@
-/* let day = 5;
+let userName = "John Doe";
+let result = userName.includes(" ");
 
-switch(day){
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thursday");
-        break;
-    case 5: 
-        console.log("Friday");
-        break;
-    case 6: 
-        console.log("Saturday");
-        break;
-    case 7:
-        console.log("Sunday");
-        break;
-    default:    
-        console.log("Invalid day");
-        break;
-} */
-
-let testScore = 100;
-let letterGrade;
-
-switch(true){
-    case testScore >= 90:
-        letterGrade = "A";
-        break;
-    case testScore >= 80:
-        letterGrade = "B";
-        break;
-    case testScore >= 70:
-        letterGrade = "C";
-        break;
-    case testScore >= 60:
-        letterGrade = "D";
-        break;
-    default:        
-        letterGrade = "F";
-        break;
-}
-
-console.log(letterGrade);
+if(result){
+    console.log("Your username cannot include a space");
+    }
+else{
+    console.log(userName);
+    }
