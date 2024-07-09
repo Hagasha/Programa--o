58206f4403c594886476ecc26481fdd6831d622c
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-09 15:45:45 -0300

    chore: Remove unnecessary form elements and update JavaScript code

--- javascript/learn.js
+++
@@ -1,7 +0,0 @@
-// let numeroTelefone = "123-456-789";
-// numeroTelefone = numeroTelefone.replaceAll("-","");
-// console.log(numeroTelefone);
-
-let numeroTelefone = "123-456-789";
-numeroTelefone = numeroTelefone.padEnd(15, "333");
-console.log(numeroTelefone);
