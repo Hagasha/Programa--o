ecc72743d9e60dec870bfac7fb828042c59dac8c
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-19 18:48:53 -0300

    feat: Update array in learn.js with new fruits and vegetables

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,12 +1,5 @@
-let fruits = ["apple", "orange", "banana", "coconut"];
+let fruits = ["Apple", "Orange", "Plum"];
+let vegetables = ["Carrot", "Cucumber", "Pepper"];
+let foods = [...fruits, ...vegetables, "eggs", "milk"];
 
-// fruits.push("pear"); para adicionar ao final
-// fruits.pop(); para excluir o último
-// fruits.unshift("mango"); para adicionar no início
-// fruits.shift(); para excluir o primeiro
-// fruits.sort();  para ordenar
-// fruits.reverse(); para inverter a ordem
-
-for(let i = fruits.length - 1; i >= 0; i--){
-    console.log(fruits[i]);
-}
+console.log(foods);
