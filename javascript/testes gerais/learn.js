b2547c2861dc7917574bdefd5ef4e468e3d29855
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-19 18:39:30 -0300

    feat: Add array manipulation functions to learn.js

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,19 +1,12 @@
-function add(x, y) {
-    return x + y;
+let fruits = ["apple", "orange", "banana", "coconut"];
+
+// fruits.push("pear"); para adicionar ao final
+// fruits.pop(); para excluir o último
+// fruits.unshift("mango"); para adicionar no início
+// fruits.shift(); para excluir o primeiro
+// fruits.sort();  para ordenar
+// fruits.reverse(); para inverter a ordem
+
+for(let i = fruits.length - 1; i >= 0; i--){
+    console.log(fruits[i]);
 }
-function subtract(x, y) {
-    return x - y;
-}
-function multiply(x, y) {
-    return x * y;
-}
-function divide(x, y) {
-    return x / y;
-}
-function isEven(number) {
-    return number % 2 === 0 ? true : false;
-}
-function isValidEmail(email) {
-    return email.includes('@') ? true : false;
-}
-console.log(isValidEmail("hagasha2@gmail.com"));
