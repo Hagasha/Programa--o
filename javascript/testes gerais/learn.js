7afcb98dbe5dd590961a5c82ec7eabbb023a2adf
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-20 16:54:01 -0300

    Refatorar learn.js para usar o operador spread nos itens de comida

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,17 +1,6 @@
-function openFridge(...foods) {
-  console.log(...foods);
-}
-function getFood(...foods) {
-    return foods;
+function combineString(...strings) {
+    return strings.join(' ');
 }
 
-const food1 = 'Pizza';
-const food2 = 'Hamburguer';
-const food3 = 'Hotdog';
-const food4 = 'Sushi';
-const food5 = 'Salad';
-
-// openFridge(food1, food2, food3, food4, food5);
-
-const foods = getFood(food1, food2, food3, food4, food5);
-console.log(foods);
+const fullName = combineString('Mr.', 'John', 'Doe');
+console.log(fullName);
