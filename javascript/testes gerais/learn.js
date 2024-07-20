7709cb84c2862fa21ac725e129521d8904295e86
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-20 16:47:40 -0300

    feat: Refactor learn.js to use spread operator for food items

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,5 +1,17 @@
-let fruits = ["Apple", "Orange", "Plum"];
-let vegetables = ["Carrot", "Cucumber", "Pepper"];
-let foods = [...fruits, ...vegetables, "eggs", "milk"];
+function openFridge(...foods) {
+  console.log(...foods);
+}
+function getFood(...foods) {
+    return foods;
+}
 
+const food1 = 'Pizza';
+const food2 = 'Hamburguer';
+const food3 = 'Hotdog';
+const food4 = 'Sushi';
+const food5 = 'Salad';
+
+// openFridge(food1, food2, food3, food4, food5);
+
+const foods = getFood(food1, food2, food3, food4, food5);
 console.log(foods);
