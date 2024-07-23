d888347f8cde256e0a6d8991fea1e110d7633d74
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-23 16:37:13 -0300

    feat: Update learn.js with forEach functions and array manipulations

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,9 +1,41 @@
-sum(displayPage, 3, 3);
+/* let numbers = [1, 2, 3, 4, 5];
 
-function sum(callback, x, y) {
-    let result = x + y;
-    callback(result);
+numbers.forEach(square);
+numbers.forEach(display);
+
+function double(element, index, array) {
+    array[index] = element * 2;
 }
-function displayPage(result) {
-    document.getElementById("myH1").textContent = result;
+
+function triple(element, index, array) {
+    array[index] = element * 3;
+}
+
+function square(element, index, array) {
+    array[index] = Math.pow(element, 2);
+}
+
+function cube(element, index, array) {
+    array[index] = Math.pow(element, 3);
+}
+
+function display(element)   {
+    console.log(element);
+} */
+
+let fruits = ['apple', 'banana', 'grape', 'orange', 'pear'];
+
+fruits.forEach(capitalize);
+fruits.forEach(display);
+
+function upperCase(element, index, array) {
+    array[index] = element.toUpperCase();
+}
+
+function capitalize(element, index, array) {
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+}
+
+function display(element) {
+    console.log(element);
 }
