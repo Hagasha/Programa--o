e8353e3d0a8060d6a812c114010480a8478c8813
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-24 17:18:08 -0300

    feat: Update learn.js with formatted dates

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,41 +1,9 @@
-/* let numbers = [1, 2, 3, 4, 5];
+const dates = ["2024-1-10", "2024-2-20", "2024-3-30"];
+const formattedDates = dates.map(formatDate);
 
-numbers.forEach(square);
-numbers.forEach(display);
+console.log(formattedDates);
 
-function double(element, index, array) {
-    array[index] = element * 2;
-}
-
-function triple(element, index, array) {
-    array[index] = element * 3;
-}
-
-function square(element, index, array) {
-    array[index] = Math.pow(element, 2);
-}
-
-function cube(element, index, array) {
-    array[index] = Math.pow(element, 3);
-}
-
-function display(element)   {
-    console.log(element);
-} */
-
-let fruits = ['apple', 'banana', 'grape', 'orange', 'pear'];
-
-fruits.forEach(capitalize);
-fruits.forEach(display);
-
-function upperCase(element, index, array) {
-    array[index] = element.toUpperCase();
-}
-
-function capitalize(element, index, array) {
-    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
-}
-
-function display(element) {
-    console.log(element);
+function formatDate(element) {
+    const parts = element.split("-");
+    return `${parts[1]}/${parts[2]}/${parts[0]}`;
 }
