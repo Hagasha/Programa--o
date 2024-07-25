ea3041b4adee325e7391a1351567f1e9d017f85c
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-25 16:02:34 -0300

    feat: Add age filtering functions to learn.js

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,9 +1,14 @@
-const dates = ["2024-1-10", "2024-2-20", "2024-3-30"];
-const formattedDates = dates.map(formatDate);
+const ages = [16, 17, 18, 19, 20, 35]
+const adults = ages.filter(isAdult)
+const children = ages.filter(isChild)
 
-console.log(formattedDates);
+console.log(adults)
+console.log(children)
 
-function formatDate(element) {
-    const parts = element.split("-");
-    return `${parts[1]}/${parts[2]}/${parts[0]}`;
+function isAdult(element) {
+  return element >= 18;
+}
+
+function isChild(element) {
+  return element < 18;
 }
