19876e8a269b1f25984948ebe6eeb81d7d9064f1
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-27 22:43:01 -0300

    feat: Update learn.js with map, filter, and console.log statements

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,9 +1,21 @@
-const prices = [5, 30, 10, 25, 15, 20];
+const numbers = [1, 2, 3, 4, 5, 6];
+const squares = numbers.map(function(element) {
+    return Math.pow(element, 2);
+});
 
-const total = prices.reduce(sum);
+const cubes = numbers.map(function(element) {
+    return Math.pow(element, 3);
+});
 
-console.log(`$${total.toFixed(2)}`);
+const evenNums = numbers.filter(function(element) {
+    return element % 2 === 0;
+});
 
-function sum(acc, element){
-    return acc + element;
-} 
+const oddNums = numbers.filter(function(element) {
+    return element % 2 !== 0;
+});
+
+console.log(squares);
+console.log(cubes);
+console.log(evenNums);
+console.log(oddNums);
