5e160144ef241d039ad8e14e10a69399843cc785
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-28 14:22:10 -0300

    feat: Update learn.js with arrow functions and array methods

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,21 +1,18 @@
+/*const hello = (name, age) => {console.log(`Hello ${name}!`)
+                        console.log(`you are ${age} years old!`)};
+hello("Rayan", 22);
+*/
+// setTimeout(() => console.log("Hello World!"), 3000);
+
 const numbers = [1, 2, 3, 4, 5, 6];
-const squares = numbers.map(function(element) {
-    return Math.pow(element, 2);
-});
-
-const cubes = numbers.map(function(element) {
-    return Math.pow(element, 3);
-});
-
-const evenNums = numbers.filter(function(element) {
-    return element % 2 === 0;
-});
-
-const oddNums = numbers.filter(function(element) {
-    return element % 2 !== 0;
-});
-
+const squares = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
+const evenNums = numbers.filter((element) => element % 2 === 0);
+const oddNums = numbers.filter((element) => element % 2 !== 0);    
+const total = numbers.reduce((acc, element) => acc + element);
+ 
 console.log(squares);
 console.log(cubes);
 console.log(evenNums);
 console.log(oddNums);
+console.log(total);
