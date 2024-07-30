08a2ca633273d02b369c145bbc0e5d823e3dc775
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-30 15:38:25 -0300

    feat: Update learn.js with pessoa1 object and methods

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,18 +1,7 @@
-/*const hello = (name, age) => {console.log(`Hello ${name}!`)
-                        console.log(`you are ${age} years old!`)};
-hello("Rayan", 22);
-*/
-// setTimeout(() => console.log("Hello World!"), 3000);
-
-const numbers = [1, 2, 3, 4, 5, 6];
-const squares = numbers.map((element) => Math.pow(element, 2));
-const cubes = numbers.map((element) => Math.pow(element, 3));
-const evenNums = numbers.filter((element) => element % 2 === 0);
-const oddNums = numbers.filter((element) => element % 2 !== 0);    
-const total = numbers.reduce((acc, element) => acc + element);
- 
-console.log(squares);
-console.log(cubes);
-console.log(evenNums);
-console.log(oddNums);
-console.log(total);
+const pessoa1 = {
+    nome: 'Luiz',
+    comidaFav: 'Pizza',
+    sayHello: function() {console.log(`Olá eu sou ${this.nome}`);},
+    eat: function() {console.log(`Eu gosto de comer ${this.comidaFav}`);}
+}; 
+pessoa1.sayHello(); 
