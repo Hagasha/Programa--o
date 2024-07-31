fdb334795a3d45374eabb48c8f1d66ea18f03811
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-07-31 13:25:05 -0300

    feat: Update learn.js with carro object and properties

--- javascript/testes gerais/learn.js
+++ javascript/testes gerais/learn.js
@@ -1,7 +1,22 @@
-const pessoa1 = {
-    nome: 'Luiz',
-    comidaFav: 'Pizza',
-    sayHello: function() {console.log(`Olá eu sou ${this.nome}`);},
-    eat: function() {console.log(`Eu gosto de comer ${this.comidaFav}`);}
-}; 
-pessoa1.sayHello(); 
+function carro(empresa, marca, modelo, ano, cor){
+    this.empresa = empresa;
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+    this.cor = cor;
+}
+
+const carro1 = new carro('Chevrolet', 'Cruze', 'LTZ', 2019, 'Preto');
+const carro2 = new carro('Fiat', 'Uno', 'Way', 2018, 'Branco');
+
+console.log(carro1.empresa)
+console.log(carro1.marca)
+console.log(carro1.modelo)
+console.log(carro1.ano)
+console.log(carro1.cor)
+
+console.log(carro2.empresa)
+console.log(carro2.marca)
+console.log(carro2.modelo)
+console.log(carro2.ano)
+console.log(carro2.cor)
