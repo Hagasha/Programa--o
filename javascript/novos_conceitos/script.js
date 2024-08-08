b47b1a899a0911eb05ec71f06aba9f1d5f0af9de
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-08-08 13:05:59 -0300

    Remove unused C# project files

--- javascript/novos_conceitos/script.js
+++ javascript/novos_conceitos/script.js
@@ -1,4 +1,9 @@
-const fruits =[{name: "Apple", color: "Red", calories: 95},
-  {name:""}
+const fruits =[{name: "Apple", color: "red", calories: 95},
+  {name:"Orange", color: "orange", calories: 45},
+  {name:"Banana", color: "yellow", calories: 105},
+  {name:"Grape", color: "purple", calories: 45},
+  {name:"Pineapple", color: "yellow", calories: 452},
+  {name:"Strawberry", color: "red", calories: 45},
+];
 
-]
+const yellowFruits = fruits.filter(fruit => fruit.color === "yellow");
