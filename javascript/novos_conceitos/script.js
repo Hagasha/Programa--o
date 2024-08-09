a2b41f693c423e76579805252983134f77391734
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-08-09 20:37:33 -0300

    Update script.js with consistent indentation and sort people array by name

--- javascript/novos_conceitos/script.js
+++ javascript/novos_conceitos/script.js
@@ -1,15 +1,9 @@
-const fruits =[{name: "Apple", color: "red", calories: 95},
-  {name:"Orange", color: "orange", calories: 45},
-  {name:"Banana", color: "yellow", calories: 105},
-  {name:"Grape", color: "purple", calories: 45},
-  {name:"Pineapple", color: "yellow", calories: 452},
-  {name:"Strawberry", color: "red", calories: 45},
-];
+const people = [
+  { name: 'John', age: 20 },
+  { name: 'Jane', age: 30 },
+  { name: 'Jim', age: 40 },
+  { name: 'Jill', age: 50 },
+]
+people.sort((a, b) => a.name.localeCompare(b.name)) // para comparar nome se usa essa função
 
-const calories = fruits.reduce((acc, fruit) => {
-  acc[fruit.color] = acc[fruit.color] || 0;
-  acc[fruit.color] += fruit.calories;
-  return acc;
-}, {});
-
-console.log(calories);
+console.log(people)
