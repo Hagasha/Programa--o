2910bf02bf21c35c21ef640962d731e1f7768192
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-08-23 15:51:07 -0300

    chore: Add Python code for string, integer, and float examples

---
+++ python/learn.py
@@ -0,0 +1,18 @@
+# Strings
+first_name = "Rayan"
+food = "Pizza"
+email = "rayan@email.com"
+
+# Integers 
+age = 20
+quantity = 3
+num_of_pets = 2
+
+# Floats
+price = 10.99
+tax = 0.07
+tip = 0.15
+
+print(f"The food is {food} and the price is {price}")
+print(f"The quantity is {quantity} and the tax is {tax}")
+print(f"The tip is {tip} and the total is {price + tax + tip}")
