6eef5e5970a5d51349c275b069881bc54d163d86
Rayan Diniz <rayandinizsilva1@gmail.com>
2024-09-03 15:09:49 -0300

    Update with logical operators in python

--- python/learn.py
+++ python/learn.py
@@ -1,3 +1,21 @@
-idade = 17
-status = "maior de idade" if idade >= 18 else "menor de idade"
-print(status)
+temp  = 20
+is_sunny = False
+
+if temp >= 28 and is_sunny:
+    print("it's Hot outside 🥵")
+    print("It's Sunny ☀️")
+elif temp <= 0 and is_sunny:
+    print("It's Cold outside 🥶")
+    print("It's Sunny ☀️")
+elif 28 > temp > 0 and is_sunny:
+    print("It's Warm outside 🙂")
+    print("It's Sunny ☀️")
+elif temp >= 28 and not is_sunny:
+    print("it's Hot outside 🥵")
+    print("It's Cloudy ☁️")
+elif temp <= 0 and not is_sunny:
+    print("It's Cold outside 🥶")
+    print("It's Cloudy ☁️")
+elif 28 > temp > 0 and not is_sunny:
+    print("It's Warm outside 🙂")
+    print("It's Cloudy ☁️")
