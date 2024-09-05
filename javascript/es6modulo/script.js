eb8a32409730e63edb5ef1fc9b401b870a840eab
Rayan Diniz <84201696+Hagasha@users.noreply.github.com>
2024-09-05 13:55:01 -0300

    feat: Add ES6 module for math calculations

---
+++ javascript/es6modulo/script.js
@@ -0,0 +1,7 @@
+import {PI, getArea, getCircumference, getVolume, getDiameter} from './math.js';
+
+console.log(PI);
+console.log(getArea(5));    
+console.log(getCircumference(5));
+console.log(getVolume(5));
+console.log(getDiameter(5));
